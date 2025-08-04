00fa0f18123622afe53083a9d4ee6299291937e5
Marco Suárez <marco16suarez@gmail.com>
2025-08-04 12:14:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Variables:
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+
+    if (nombre === "") {
+        alert("Por favor ingresar un nombre válido");
+    } else {
+        listaAmigos.push(nombre);
+    }
+    limpiarCaja();
+    return;
+}
+
+agregarAmigo();
+
+function limpiarCaja() {
+    valorCaja = document.getElementById("amigo").value = "";
+}
